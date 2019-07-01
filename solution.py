0c8c54116adea1ab035871ada55c5a13180db55b
Vladislav Belavin <belavin@phystech.edu>
2019-07-01 07:59:50 +0000

    adding comments

--- solution.py
+++ solution.py
@@ -5,12 +5,14 @@ import numpy as np
 def main():
     input_dir, output_dir = sys.argv[1:]
     
+    # how to load data
     data_val = np.load(input_dir + '/data_val.npz', allow_pickle=True)
     N_val = len(data_val['EnergyDeposit'])
 
     data_test = np.load(input_dir + '/data_test.npz', allow_pickle=True)
     N_test = len(data_test['EnergyDeposit'])
 
+    # just some random predictions, still valid
     np.savez_compressed(output_dir + '/data_val_prediction.npz', 
                         ParticlePoint=np.random.randn(N_val, 2), 
                         ParticleMomentum=np.random.randn(N_val, 2))
